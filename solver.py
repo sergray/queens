be656f87755916a08060815b382f6e01821fd9c5
Sergey Panfilov <sergey.panfilov@kpn.com>
2015-11-20 06:59:43 +0100

    Make x - rows and y - columns as in spec

--- solver.py
+++ solver.py
@@ -2,13 +2,13 @@ def compute(rows, columns, max_queens_on_sight, initial_queens):
     field = [[0 for j in range(columns)] for i in range(rows)]
     for queen in initial_queens:
         x = queen['x']; y = queen['y']
-        field[y][x] = 1
+        field[x][y] = 1
     added_queens = set()
     # naive solution without backtracking
     i = 0; j = 0
     while j < columns:
-        if can_add(j, i, field, max_queens_on_sight):
-            added_queens.add((j, i))
+        if can_add(i, j, field, max_queens_on_sight):
+            added_queens.add((i, j))
             field[i][j] = 1
         i += 1
         if i >= rows:
@@ -19,7 +19,7 @@ def compute(rows, columns, max_queens_on_sight, initial_queens):
 
 
 def can_add(x, y, field, max_queens_on_sight):
-    return not field[y][x] and on_sight_count(x, y, field) <= max_queens_on_sight
+    return not field[x][y] and on_sight_count(x, y, field) <= max_queens_on_sight
 
 
 def on_sight_count(x, y, field):
@@ -39,26 +39,26 @@ def on_sight_count(x, y, field):
     cols = len(field[0])
     count = 0
     # in a row
-    count += sum(field[y]) - field[y][x]
+    count += sum(field[x]) - field[x][y]
     # in a column
-    count += sum(field[i][x] for i in range(rows)) - field[y][x]
+    count += sum(field[i][y] for i in range(rows)) - field[x][y]
     # left-up
-    i = y - 1; j = x - 1
+    i = x - 1; j = y - 1
     while i >= 0 and j >= 0:
         count += field[i][j]
         i -= 1; j -= 1
     # right-down
-    i = y + 1; j = x + 1
+    i = x + 1; y = x + 1
     while i < rows and j < cols:
         count += field[i][j]
         i += 1; j += 1
     # left-down
-    i = y + 1; j = x - 1
+    i = x + 1; j = y - 1
     while i < rows and j >= 0:
         count += field[i][j]
         i += 1; j -= 1
     # right-up
-    i = y - 1; j = x + 1
+    i = x - 1; j = y + 1
     while i >= 0 and j < cols:
         count += field[i][j]
         i -= 1; j += 1
